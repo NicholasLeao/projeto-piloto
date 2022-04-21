627db8f9007bb61d76b27a8bdf8d19b400a65b29
NicholasLeao <tropicaldata@gmail.com>
2022-04-21 16:42:40 -0300

    initial form implementation

---
+++ src/components/Formulario.js
@@ -0,0 +1,116 @@
+import axios from "axios";
+function Formulario(props) {
+  // EVENT HANDLERS
+  const handleChange = (e) => {
+    // SET NEW STATE
+    props.setFormularioState((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+    console.log(props.formularioState);
+  };
+
+  const handleClick = (e) => {
+    // PREVENIR DEFAULT
+    e.preventDefault();
+
+    // CHECAGEM DOS CAMPOS
+
+    if (!/(.+)@(.+){2,}\.(.+){2,}/.test(props.formularioState.email)) {
+      alert("email errado");
+      return;
+    }
+
+    if (!/\w\s+/.test(props.formularioState.tags)) {
+      alert("tags erradas");
+      return;
+    }
+
+    // SUBMIT FORMULARIO MANDAR PARA API
+    props.setSubmitFormularioState(props.formularioState);
+    props.setFormularioState({
+      titulo: "",
+      descricao: "",
+      corpo: "",
+      email: "",
+      telefone: "",
+      tags: [],
+    });
+    axios.post(
+      "https://ironrest.herokuapp.com/projetopilotowillnick",
+      props.submitFormularioState
+    );
+  };
+
+  // ==== JSX ================================
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="titulo">Titulo</label>
+          <input
+            onChange={handleChange}
+            maxLength="45"
+            type="text"
+            name="titulo"
+            value={props.formularioState.titulo}
+            placeholder="Titulo do post"
+          ></input>
+          <label htmlFor="descricao">Descricao</label>
+          <input
+            onChange={handleChange}
+            maxLength="70"
+            type="text"
+            name="descricao"
+            value={props.formularioState.descricao}
+            placeholder="Breve descricao"
+          ></input>
+          <label htmlFor="corpo">Corpo</label>
+          <textarea
+            onChange={handleChange}
+            type="textarea"
+            maxLength="280"
+            name="corpo"
+            value={props.formularioState.corpo}
+            placeholder="Descricao do post"
+          ></textarea>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            maxLength="45"
+            name="email"
+            value={props.formularioState.email}
+            placeholder="Email"
+          ></input>
+          <label htmlFor="telefone">Telefone</label>
+          <input
+            onChange={handleChange}
+            type="number"
+            maxLength="12"
+            name="telefone"
+            value={props.formularioState.telefone}
+            placeholder="Telefone"
+          ></input>
+          <label htmlFor="tags">Tags</label>
+          <p>
+            <small>(separe por espacos)</small>
+          </p>
+          <input
+            onChange={handleChange}
+            type="text"
+            maxLength="35"
+            name="tags"
+            value={props.formularioState.tags}
+            placeholder="Tags"
+          ></input>
+          <button onClick={handleClick}>Submit</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Formulario;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,40 @@
 import CardPequeno from "../components/CardPequeno";
-import styles from "../styles/Home.module.css"
+import Formulario from "../components/Formulario";
+import CardGrande from "../components/CardGrande";
+import Modal from "../components/Modal";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect, useCallback } from "react";
 
 function Home() {
-    return ( 
-        <>
-        <main className={styles["home"]}>
-            <div >
-                <CardPequeno/>
-            </div>
-        </main>
-        </>
-     );
+  // STATES
+  const [submitFormularioState, setSubmitFormularioState] = useState();
+  const [formularioState, setFormularioState] = useState({
+    titulo: "",
+    descricao: "",
+    corpo: "",
+    email: "",
+    telefone: "",
+    tags: [],
+  });
+
+  // ==== JSX ================================
+  return (
+    <>
+      <main className={styles["home"]}>
+        <div>
+          <CardPequeno />
+          <Formulario
+            submitFormularioState={submitFormularioState}
+            setSubmitFormularioState={setSubmitFormularioState}
+            formularioState={formularioState}
+            setFormularioState={setFormularioState}
+          />
+          <CardGrande />
+          <Modal />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;
